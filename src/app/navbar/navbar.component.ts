d44f384638a3b81483b3171542b71e30672fcade
Brayan Duque <bradumar2016@gmail.com>
2024-06-03 17:03:09 -0500

    about v1

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -12,7 +12,7 @@ import { CommonModule } from '@angular/common';
 })
 export class NavbarComponent {
 
-  isActive = true;
+  isActive = false;
 
   toggleNavbar() {
       this.isActive = !this.isActive;
