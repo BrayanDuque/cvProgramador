50b5d8178a9163017ada303ce6b921d842ef2c73
Brayan Duque <bradumar2016@gmail.com>
2024-06-03 01:48:00 -0500

    cv programador v1

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,24 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { ProyectsComponent } from './proyects/proyects.component';
+import { ContactComponent } from './contact/contact.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path:"",
+        component: HomeComponent
+    },
+    {
+        path:"about",
+        component: AboutComponent
+    },
+    {
+        path:"proyects",
+        component: ProyectsComponent
+    },
+    {
+        path: "contact",
+        component: ContactComponent
+    }
+];
---
+++ src/app/navbar/navbar.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+import { RouterLinkWithHref, RouterLink, RouterLinkActive } from '@angular/router';
+import { AboutComponent } from '../about/about.component';
+import { CommonModule } from '@angular/common';
+
+
+@Component({
+  selector: 'app-navbar',
+  standalone: true,
+  imports: [RouterLinkWithHref, RouterLink, CommonModule, RouterLinkActive],
+  templateUrl: './navbar.component.html',
+  styleUrl: './navbar.component.css'
+})
+export class NavbarComponent {
+
+}
